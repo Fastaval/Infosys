58b4bbdd21e3eb147919c49c3fa6c01da32a724a
Mikkel Westh <westh_mikkel@hotmail.com>
2022-09-07 18:19:49 +0200

    ADDED List item CHANGED separated tool bar to it's own class CHANGED separated text preprosessor to own class ADDED Signup page "organizer" ADDED Signup page "practical" FIXED improved logic for inserting into selection FIXED added user check to SignupAdminController

---
+++ public/js/signup/preprocess.js
@@ -0,0 +1,26 @@
+"using strict";
+
+class InfosysTextPreprocessor {
+  // Preprocess text
+  static process_text(text) {
+    text = jQuery('<div>'+text+'</div>').text(); //strip any HTML
+    for (let match of text.matchAll(/\[(\w+)\](.+?)\[\/\1\]/gs)) {
+      switch (match[1]) {
+        case "email":
+          text = text.replace(match[0], '<a href="mailto:'+match[2]+'">'+match[2]+'</a>');
+          break;
+
+        case "b":
+          text = text.replace(match[0], '<strong>'+match[2]+'</strong>');
+          break;
+
+        default:
+          console.log("Unknown token", match);
+          break;
+      }
+    }
+    text = text.replaceAll("\n", "<br>");
+    
+    return text;
+  }
+}
