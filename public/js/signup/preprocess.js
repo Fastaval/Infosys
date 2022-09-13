c3d3d2a2e551bf02af07484d6353799261d403f7
Mikkel Westh <westh_mikkel@hotmail.com>
2022-09-13 23:11:24 +0200

    ADDED url as markup

--- public/js/signup/preprocess.js
+++ public/js/signup/preprocess.js
@@ -4,14 +4,19 @@ class InfosysTextPreprocessor {
   // Preprocess text
   static process_text(text) {
     text = jQuery('<div>'+text+'</div>').text(); //strip any HTML
-    for (let match of text.matchAll(/\[(\w+)\](.*?)\[\/\1\]/gs)) {
+    for (let match of text.matchAll(/\[(\w+)(?:=([^\]]+))?\](.*?)\[\/\1\]/gs)) {
       switch (match[1]) {
         case "email":
-          text = text.replace(match[0], '<a href="mailto:'+match[2]+'">'+match[2]+'</a>');
+          text = text.replace(match[0], '<a href="mailto:'+match[3]+'">'+match[3]+'</a>');
           break;
 
         case "b":
-          text = text.replace(match[0], '<strong>'+match[2]+'</strong>');
+          text = text.replace(match[0], '<strong>'+match[3]+'</strong>');
+          break;
+
+        case "url":
+          let url = match[2].replaceAll('"','\\"');
+          text = text.replace(match[0], '<a href="'+url+'">'+match[3]+'</a>');
           break;
 
         default:
